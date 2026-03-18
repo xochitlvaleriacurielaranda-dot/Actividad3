f662d400baf345f26b2e00b3e2379213aa71e47e
Valeria Curiel <xochitlvaleriacurielaranda@gmail.com>
2026-03-18 14:20:36 -0600

    segundo commit

--- src/Problema1/Composicion.java
+++ src/Problema1/Composicion.java
@@ -1,4 +1,52 @@
 package Problema1;
-
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 public class Composicion {
-}
+        private String titulo;
+        private Duration duracion;
+        private String genero;
+        private LocalDate fechaRegistro;
+        private LocalDate fechaEstreno;
+        private List<Solicitud> solicitudes;
+
+        public Composicion(String titulo, Duration duracion, String genero, LocalDate fechaRegistro, LocalDate fechaEstreno) {
+            this.titulo = titulo;
+            this.duracion = duracion;
+            this.genero = genero;
+            this.fechaRegistro = fechaRegistro;
+            this.fechaEstreno = fechaEstreno;
+            this.solicitudes = new ArrayList<Solicitud>();
+        }
+
+        public void addSolicitud(Solicitud solicitud){this.solicitudes.add(solicitud);}
+
+        public void removeSolicitud(Solicitud solicitud){this.solicitudes.remove(solicitud);}
+
+        public List<String> getInterpretes() {
+            List<String> nombres = new ArrayList<>();
+            for (Solicitud s : solicitudes) {
+                nombres.add(s.getNombre());
+            }
+            return nombres;
+        }
+
+        public String getTitulo() {return titulo;}
+        public Duration getDuracion() {return duracion;}
+        public String getGenero() {return genero;}
+        public LocalDate getFechaRegistro() {return fechaRegistro;}
+        public LocalDate getFechaEstreno() {return fechaEstreno;}
+        public List<Solicitud> getSolicitudes() {return solicitudes;}
+
+        @Override
+        public String toString() {
+            return "Composicion{" +
+                    "Titulo='" + titulo + '\'' +
+                    ", Duracion=" + duracion +
+                    ", Genero='" + genero + '\'' +
+                    ", Fecha de Registro=" + fechaRegistro +
+                    ", Fecha de Estreno=" + fechaEstreno +
+                    '}';
+        }
+    }
--- src/Problema1/Solicitud.java
+++ src/Problema1/Solicitud.java
@@ -1,4 +1,46 @@
 package Problema1;
-
+import java.time.LocalDate;
 public class Solicitud {
-}
+        private String nombre;
+        private LocalDate fechaSolicitud;
+        private LocalDate fechaAutorizacion;
+        private String compania;
+        private Composicion composicion;
+
+        public Solicitud(String nombre, LocalDate fechaSolicitud, LocalDate fechaAutorizacion, String compania, Composicion composicion) {
+            if (fechaAutorizacion.isBefore(fechaSolicitud)){throw new IllegalArgumentException("Error en la fecha de autorizacion");}
+            this.nombre = nombre;
+            this.fechaSolicitud = fechaSolicitud;
+            this.fechaAutorizacion = fechaAutorizacion;
+            this.compania = compania;
+            this.composicion = composicion;
+
+            composicion.addSolicitud(this);
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public LocalDate getFechaSolicitud() {
+            return fechaSolicitud;
+        }
+
+        public LocalDate getFechaAutorizacion() {
+            return fechaAutorizacion;
+        }
+
+        public String getCompania() {
+            return compania;
+        }
+
+        @Override
+        public String toString() {
+            return "Solicitud{" +
+                    "Nombre='" + nombre + '\'' +
+                    ", Fecha de Solicitud=" + fechaSolicitud +
+                    ", Fecha de Autorizacion=" + fechaAutorizacion +
+                    ", Compania='" + compania + '\'' +
+                    '}';
+        }
+    }
